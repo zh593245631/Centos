e7496dc054a6484e4c0825aac6a5715ec6f2c380
zh593245631 <593245631@qq.com>
2019-04-29 02:29:47 -0700

    gcc -E -S -c

---
+++ 19-4-29/test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define M 100
+
+int main()
+{
+  printf("%d",M);//打印
+  return 0;
+}
---
+++ 19-4-29/test1/test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define ADD(x,y) (x+y)
+
+int main()
+{
+  printf("%d\n",ADD(2,3)*3);//ADD()
+  return 0;
+}
