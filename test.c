49a97450e18704382f76446d398d9756d8132544
zh593245631 <593245631@qq.com>
2019-04-29 18:28:42 -0700

    B B B B A 带颜色的# 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交 A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A

---
+++ test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+int main()
+{
+  const char* a = "/~\\";
+  char buf[103] = {0};
+
+  memset(buf,' ',101);
+  buf[101] = ']';
+  buf[102] = '\0';
+  int i = 0;
+  for(; i<=100; i++)
+  {
+    buf[i] = '#';
+    printf(" \e[0;31m[%d%%]\e[1;33m[%c]\e[1;32m[%s\r",i,a[i%3],buf);
+    fflush(stdout);
+    usleep(500*1000);
+  }
+  printf("\ncomplete\n\e[0m");
+  return 0;
+}
