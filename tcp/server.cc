9409902a922360fb6a7123a72ce65f8604f0ebbd
zh593245631 <593245631@qq.com>
2019-06-16 14:52:46 +0800

    tcp可以自定义ip

--- tcp/server.cc
+++ tcp/server.cc
@@ -17,12 +17,16 @@ void handler(const string& req, string* resq)
   *resq = it->second;
     clog<<*resq<<endl;
 }
-int main()
+int main(int argc, char* argv[])
 {
+  if(argc != 3){
+    printf("请输入 [ip] [port]\n");
+    return 0;
+  }
   dict.insert(make_pair("hehe","呵呵"));
   dict.insert(make_pair("hi","嗨"));
   dict.insert(make_pair("hello","你好"));
-  TcpServer s("192.168.199.129",9090);
+  TcpServer s(argv[1],atoi(argv[2]));
   s.Start(handler);
   return 0;
 }
