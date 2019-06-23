1fe30e7c143881e7c628e3b80de99409126e36c2
zh593245631 <593245631@qq.com>
2019-06-23 22:44:28 +0800

    函数压栈

---
+++ p.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int i = 0;
+  cout<<++i<<" "<<++i<<endl;
+  return 0;
+}
---
+++ q.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+  int i = 0;
+  printf("%d %d\n",++i,++i);
+  return 0;
+}
